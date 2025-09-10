2db0d38a823162c55b8c45e17a151e1a5b52e995
toyota-m2k <toyota.mitsuki@gmail.com>
2025-09-10 17:40:36 +0900

    libs: min dependency
    chore: agp 8.13.0

--- libLogger/build.gradle.kts
+++ libLogger/build.gradle.kts
@@ -42,10 +42,7 @@ kotlin {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
